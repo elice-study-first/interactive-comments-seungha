d5a3a114fe7834601158a5f75b58bbcdeb192186
lilyoh <lilyoh628@gmail.com>
2023-10-04 22:58:46 +0900

    FEAT: add class name to wrap image and textarea & add keydown eventListener

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,20 +1,22 @@
 import Component from '../../core/Component';
 import { html } from '../../helper';
+import './CommentForm.css';
 
 export default class CommentForm extends Component {
 	template() {
 		return html`
 			<form class="comment-form">
-				<img
-					class="user-avatar"
-					src="${this.props.currentUser.image.png}"
-					alt="user avatar"
-				/>
-				<textarea
-					name="content"
-					class="comment-form--content"
-				>
-				</textarea>
+				<div class="comment-form-left">
+					<img
+						class="user-avatar"
+						src="${this.props.currentUser.image.png}"
+						alt="user avatar"
+					/>
+					<textarea
+						name="content"
+						class="comment-form--content"
+					></textarea>
+				</div>
 				<button
 					class="comment-form--button"
 					type="submit"
@@ -30,6 +32,13 @@ export default class CommentForm extends Component {
 			event.preventDefault();
 			const newComment = event.target.elements.content.value;
 			this.props.addComment(newComment);
+		});
+
+		this.addEvent('keydown', '.comment-form--content', (event) => {
+			if (event.key === 'Enter' && !event.shiftKey) {
+				const newComment = event.target.value;
+				this.props.addComment(newComment);
+			}
 		});
 	}
 }
