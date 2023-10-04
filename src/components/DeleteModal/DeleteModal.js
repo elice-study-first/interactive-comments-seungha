b1be5b22013e5bc93106f35d3d74385ff2ed9ec8
lilyoh <lilyoh628@gmail.com>
2023-10-05 01:58:38 +0900

    FEAT: styling delete modal
    resolve #12

--- src/components/DeleteModal/DeleteModal.js
+++ src/components/DeleteModal/DeleteModal.js
@@ -1,5 +1,6 @@
 import { html } from '../../helper';
 import Component from '../../core/Component';
+import './DeleteModal.css';
 
 export default class DeleteModal extends Component {
 	template() {
@@ -7,11 +8,12 @@ export default class DeleteModal extends Component {
 			<div class="delete-modal--container">
 				<div class="delete-modal">
 					<h1 class="delete-modal--title">Delete comment</h1>
-					<p class="delete-modal-text">
+					<p class="delete-modal--text">
 						Are you sure you want to delete this comment? This will remove the comment and can't be undone.
 					</p>
-					<div class="delete-modal-button-wrapper">
-						<button class="no">NO, CANCEL</button><button class="yes">YES, DELETE</button>
+					<div class="delete-modal-button--wrapper">
+						<button class="delete-modal-button no">NO, CANCEL</button>
+						<button class="delete-modal-button yes">YES, DELETE</button>
 					</div>
 				</div>
 			</div>
